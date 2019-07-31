db03e100c838d3fea82eced8cc6fa93892c837f4
Hung Vu <hungvmgch17180@fpt.edu.vn>
2019-07-31 11:09:13 +0700

    asda

--- index.php
+++ index.php
@@ -7,6 +7,15 @@
 	<?php
 	echo "I Miss You 3000 !!!";
 	?>
+<table border="0" cellpadding="10">
+
+	<tr>
+		<td><img src="images/a2.jpg" alt=""  width = "50%" height = "600px"></td>
+		<td>Vu Manh Hung</td>
+		<td>DOTA</td>
+	</tr>
+
+</table>
 
 
 </body>
